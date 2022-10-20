c326f85c32a703331764f895264ae97f43927203
IntoTheForrest <116296847+IntoTheForrest@users.noreply.github.com>
2022-10-20 12:17:59 -0400

    Initial Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,26 @@
+export default [
+	{
+		title: 'Terminal Website',
+		imgUrl: 'src/assets/terminal-website.jpg',
+		stack: ['HTML', 'CSS', 'JavaScript'],
+		workUrl: 'https://fkcodes.com',
+	},
+	{
+		title: 'Developer Portfolio',
+		imgUrl: 'src/assets/dev-portfolio.jpg',
+		stack: ['React', 'Tailwind', 'Vite'],
+		workUrl: '#',
+	},
+	{
+		title: 'AI Nerf Gun',
+		imgUrl: 'src/assets/ai-nerf-gun.jpg',
+		stack: ['Python', 'C++', 'Robotics'],
+		workUrl: 'https://youtu.be/A8eegxL82PI',
+	},
+	// {
+	// 	title: 'Proprietary App',
+	// 	imgUrl: 'src/assets/dislike-count.jpg',
+	// 	stack: ['Java', 'TypeScript', 'Spring'],
+	// 	workUrl: 'https://youtu.be/1uAH93tzfQY',
+	// },
+];
